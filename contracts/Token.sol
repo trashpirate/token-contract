1a45612e600567092add6d0542a47bd09c3e863e
trashpirate <nadinaz@sent.com>
2022-10-27 08:42:55 -0500

    add Taste contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,131 +2,48 @@
 pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token {
+contract RFCTToken is ERC20, Ownable {
+    
 
-    string public name;
-    string public symbol;
-    uint256 public decimals = 18;
-    uint256 public totalSupply;
+    mapping (address => uint256) private _rOwned; // reflection amount
+    mapping (address => uint256) private _tOwned; // token amount
 
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping (address => mapping (address => uint256)) private _allowances;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address spender, uint256 value);
+    mapping (address => bool) private _isExcludedFromFee;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _totalSupply
-    ) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _totalSupply * (10**decimals);
+    mapping (address => bool) private _isExcluded;
+    address[] private _excluded;
+   
+    uint256 private constant MAX = ~uint256(0);
+    uint256 private _tTotal;
+    uint256 private _rTotal;
+    uint256 private _tFeeTotal;
 
-        // assign total supply to deployer
-        balanceOf[msg.sender] = totalSupply;
-    }
+    string private _name;
+    string private _symbol;
+    uint256 private _decimals;
+    
+    uint256 public _taxFee;
+    uint256 private _previousTaxFee;
+    
+    uint256 public _liquidityFee;
+    uint256 private _previousLiquidityFee;
 
+    IUniswapV2Router02 public immutable uniswapV2Router;
+    address public immutable uniswapV2Pair;
+    
+    bool inSwapAndLiquify;
+    bool public swapAndLiquifyEnabled = true;
+    
+    uint256 public _maxTxAmount;
+    uint256 public numTokensSellToAddToLiquidity;
+    
 
-    /** INTERNAL FUNCTIONS */
-    function _mint(address account, uint256 amount) internal {
-        
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        totalSupply += amount;
-
-        unchecked {
-            // Overflow not possible: balance + amount is at most totalSupply + amount, which is checked above.
-            balanceOf[account] += amount;
-        }
-        emit Transfer(address(0), account, amount);
-
-    }
-
-    function _transfer(
-        address _from,
-        address _to,
-        uint256 _value
-    ) internal {
-        require(_to != address(0));
-
-        unchecked {
-            // Deduct tokens from spender
-            balanceOf[_from] -= _value;
-            // Credit tokens to receiver
-            balanceOf[_to] += _value;
-        }
-
-        //Emit event
-        emit Transfer(_from, _to, _value);
-    }
-
-    function _approve(address _owner, address _spender, uint256 _amount)
-        internal
-    {
-        require(_owner != address(0), "ERC20: approve from the zero address");
-        require(_spender != address(0), "ERC20: approve to the zero address");
-
-        allowance[_owner][_spender] = _amount;
-
-        emit Approval(_owner, _spender, _amount);
-    }
-
-    function _spendAllowance(
-        address _owner,
-        address _spender,
-        uint256 _amount
-    ) internal {
-        uint256 _currentAllowance = allowance[_owner][_spender];
-        if (_currentAllowance != type(uint256).max) {
-            require(_currentAllowance >= _amount, "ERC20: insufficient allowance");
-            require(balanceOf[_owner] >= _amount, "ERC20: insufficient allowance");
-
-            unchecked {
-                _approve(_owner, _spender, _currentAllowance - _amount);
-            }
-        }
-    }
-
-    /** PUBLIC ERC20 COMPLIENT FUNCTIONS */
-
-    function transfer(address _to, uint256 _value)
-        public
-        returns (bool success)
-    {
-        // Require that sender is valid and has enough tokens
-        require(balanceOf[msg.sender] >= _value);
-        _transfer(msg.sender, _to, _value);
-        
-        return true;
-    }
-
-    function approve(address _spender, uint256 _amount)
-        public
-        returns (bool success)
-    {
-        address _owner = msg.sender;
-        _approve(_owner, _spender, _amount);
-        return true;
-    }
-
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) 
-        public 
-        returns (bool success) 
-    {   
-
-        address _spender = msg.sender;
-        _spendAllowance(_from, _spender, _amount);
-
-        // spend tokens
-        _transfer(_from, _to, _amount);
-        
-        return true;
+    constructor(uint256 _supply) ERC20("Reflection", "RFCT") {
+        _mint(msg.sender, initialSupply);
     }
 }
