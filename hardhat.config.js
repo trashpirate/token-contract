cc8512e2597afdc91b4fd450059208a7b85f2d8f
trashpirate <nadinaz@sent.com>
2022-10-31 12:52:14 -0500

    add bep-20 deployment config

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,8 +13,25 @@ module.exports = {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(","),
     },
+    bsc_test: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: privateKeys.split(","),
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: privateKeys.split(","),
+    },
   },
   etherscan: {
+    // verification on Ethereum
     apiKey: `${process.env.ETHERSCAN_API_KEY}`,
   },
+  // etherscan: {
+  //   // verification on Binance Smart Chain
+  //   apiKey: `${process.env.BSCSCAN_API_KEY}`,
+  // },
 };
