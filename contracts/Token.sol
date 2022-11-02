6c41d7756d9e45765dd171f017d89be6121ea39d
trashpirate <nadinaz@sent.com>
2022-11-01 22:34:04 -0500

    update total supply

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,11 +8,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PeerSharePlace is ERC20, Ownable {
 
-    uint256 constant _initial_supply = 1000000000 * (10**18);
+    uint256 constant private _initial_supply = 1_000_000_000 * (10**18);
 
     constructor() ERC20("PeerSharePlace", "BLOCK") {
         
         _mint(msg.sender,_initial_supply);
         
     }
+
 }
